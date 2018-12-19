2a701b2281a1a377853d9f9ff429011f6fcd03cb
ying1109 <921172520@qq.com>
2018-12-19 23:16:22 +0800

    学习（HTTP路由，安装postman，HTTP控制器）

---
+++ app/Http/Controllers/Admin/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/12/19
+ * Time: 22:23
+ */
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller {
+    public function index() {
+        echo 123;
+    }
+}
